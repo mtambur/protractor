496fab287dab7931e2bf81ff9a62dc2eac6ea7c5
zivomri <omriziv12@gmail.com>
2017-09-13 11:16:40 +0300

    Update multi.ts

--- exampleTypescript/multi.ts
+++ exampleTypescript/multi.ts
@@ -3,8 +3,14 @@ import {AngularHomepage} from './angularPage';
 
 // The jasmine typings are brought in via DefinitelyTyped ambient typings.
 describe('angularjs homepage', () => {
+  beforeEach(() => {
+      console.log('beforeEach');
+      browser.sleep(5000)
+
+  });
+
+
   it('should greet the named user 1', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -12,7 +18,6 @@ describe('angularjs homepage', () => {
   });
   
   it('should greet the named user 2', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -21,7 +26,6 @@ describe('angularjs homepage', () => {
 
 
   it('should greet the named user 3', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -30,7 +34,6 @@ describe('angularjs homepage', () => {
 
 
   it('should greet the named user 4', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -39,7 +42,6 @@ describe('angularjs homepage', () => {
 
 
   it('should greet the named user 5', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -47,7 +49,6 @@ describe('angularjs homepage', () => {
   });
 
   it('should greet the named user 6', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -56,7 +57,6 @@ describe('angularjs homepage', () => {
 
 
   it('should greet the named user 7', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -65,7 +65,6 @@ describe('angularjs homepage', () => {
 
 
   it('should greet the named user 8', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -74,7 +73,6 @@ describe('angularjs homepage', () => {
 
 
   it('should greet the named user 9', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -82,7 +80,6 @@ describe('angularjs homepage', () => {
   });
 
   it('should greet the named user 10', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -90,7 +87,6 @@ describe('angularjs homepage', () => {
   });
 
   it('should greet the named user 11', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -98,7 +94,6 @@ describe('angularjs homepage', () => {
   });
 
   it('should greet the named user 12', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -106,7 +101,6 @@ describe('angularjs homepage', () => {
   });
 
   it('should greet the named user 13', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -114,7 +108,6 @@ describe('angularjs homepage', () => {
   });
 
   it('should greet the named user 14', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
@@ -122,70 +115,60 @@ describe('angularjs homepage', () => {
   });
 
   it('should greet the named user 15', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
     expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
   });
   it('should greet the named user 16', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
     expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
   });
   it('should greet the named user 17', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
     expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
   });
   it('should greet the named user 18', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
     expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
   });
   it('should greet the named user 19', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
     expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
   });
   it('should greet the named user 20', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
     expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
   });
   it('should greet the named user 21', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
     expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
   });
   it('should greet the named user 22', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
     expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
   });
   it('should greet the named user 23', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
     expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
   });
   it('should greet the named user 24', () => {
-    browser.sleep(5000)
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
     angularHomepage.setName('Julie');
