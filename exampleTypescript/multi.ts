17bbe1d5e52bed8158512ed8202767804b403045
zivomri <omriziv12@gmail.com>
2017-09-13 11:27:43 +0300

    Update multi.ts

--- exampleTypescript/multi.ts
+++ exampleTypescript/multi.ts
@@ -12,46 +12,46 @@ describe('angularjs homepage', () => {
   it('should greet the named user 1', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
   
   it('should greet the named user 2', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 
 
   it('should greet the named user 3', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 
 
   it('should greet the named user 4', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 
 
   it('should greet the named user 5', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 
   it('should greet the named user 6', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 
 
@@ -66,111 +66,111 @@ describe('angularjs homepage', () => {
   it('should greet the named user 8', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 
 
   it('should greet the named user 9', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 
   it('should greet the named user 10', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 
   it('should greet the named user 11', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 
   it('should greet the named user 12', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 
   it('should greet the named user 13', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 
   it('should greet the named user 14', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 
   it('should greet the named user 15', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
   it('should greet the named user 16', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
   it('should greet the named user 17', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
   it('should greet the named user 18', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
   it('should greet the named user 19', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
   it('should greet the named user 20', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
   it('should greet the named user 21', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
   it('should greet the named user 22', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
   it('should greet the named user 23', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
   it('should greet the named user 24', () => {
     let angularHomepage = new AngularHomepage();
     angularHomepage.get();
-    angularHomepage.setName('Julie');
-    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+    angularHomepage.setName('Omri');
+    expect(angularHomepage.getGreeting()).toEqual('Hello Omri!');
   });
 });
