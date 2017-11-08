fa70b5c6a4e5efa7a38f42ef0bbd645065ccb727
mtambur <mtambur@gmail.com>
2017-11-08 17:41:02 +0200

    Create spec01.mix.ts

---
+++ exampleTypescript/spec01.mix.ts
@@ -0,0 +1,110 @@
+// Multiple spec files for CDD TAR wildcard import and execution
+import {browser, element, by, By, $, $$, ExpectedConditions} from 'protractor';
+
+describe('CDD TAR all TC PASS 01 A', () => {
+  beforeEach(() => {
+    browser.get('http://www.angularjs.org');
+  });
+
+  it('should greet the named user 01 A', () => {
+    element(by.model('yourName')).sendKeys('Julie');
+    let greeting = element(by.binding('yourName'));
+    expect(greeting.getText()).toEqual('Hello Julie!');
+  });
+
+  it('should list todos 01 A', function() {
+    let todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(2);
+    expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
+  });
+});
+
+describe('CDD TAR all tc fail 01 A', () => {
+  beforeEach(() => {
+    browser.get('http://www.angularjs.org');
+  });
+
+  it('should greet the named user 01 A', () => {
+    element(by.model('yourName')).sendKeys('Julie');
+    let greeting = element(by.binding('yourName'));
+    expect(greeting.getText()).toEqual('FAIL A!');
+  });
+
+  it('should list todos 01 A', function() {
+    let todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(2);
+    expect(todoList.get(1).getText()).toEqual('FAIL A');
+  });
+});
+
+describe('CDD TAR all TC PASS 01 B', () => {
+  beforeEach(() => {
+    browser.get('http://www.angularjs.org');
+  });
+
+  it('should greet the named user 01 B', () => {
+    element(by.model('yourName')).sendKeys('Julie');
+    let greeting = element(by.binding('yourName'));
+    expect(greeting.getText()).toEqual('Hello Julie!');
+  });
+
+  it('should list todos 01 B', function() {
+    let todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(2);
+    expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
+  });
+});
+
+describe('CDD TAR all tc fail 01 B', () => {
+  beforeEach(() => {
+    browser.get('http://www.angularjs.org');
+  });
+
+  it('should greet the named user 01 B', () => {
+    element(by.model('yourName')).sendKeys('Julie');
+    let greeting = element(by.binding('yourName'));
+    expect(greeting.getText()).toEqual('FAIL B!');
+  });
+
+  it('should list todos 01 B', function() {
+    let todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(2);
+    expect(todoList.get(1).getText()).toEqual('FAIL B');
+  });
+});
+
+describe('CDD TAR all TC PASS 01 C', () => {
+  beforeEach(() => {
+    browser.get('http://www.angularjs.org');
+  });
+
+  it('should greet the named user 01 C', () => {
+    element(by.model('yourName')).sendKeys('Julie');
+    let greeting = element(by.binding('yourName'));
+    expect(greeting.getText()).toEqual('Hello Julie!');
+  });
+
+  it('should list todos 01 C', function() {
+    let todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(2);
+    expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
+  });
+});
+
+describe('CDD TAR all tc fail 01 C', () => {
+  beforeEach(() => {
+    browser.get('http://www.angularjs.org');
+  });
+
+  it('should greet the named user 01 C', () => {
+    element(by.model('yourName')).sendKeys('Julie');
+    let greeting = element(by.binding('yourName'));
+    expect(greeting.getText()).toEqual('FAIL B!');
+  });
+
+  it('should list todos 01 C', function() {
+    let todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(2);
+    expect(todoList.get(1).getText()).toEqual('FAIL B');
+  });
+});
