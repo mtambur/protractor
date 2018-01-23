9e01a4d415fac34ce703b20c39b3d25ceb0bdae8
mtambur <mtambur@gmail.com>
2018-01-23 16:46:28 +0200

    Create conf.suites.multitc.pass.FF.ts

---
+++ exampleTypescript/conf.suites.multitc.pass.FF.ts
@@ -0,0 +1,21 @@
+// CDD TAR
+// conf suites file with all ts specs
+import {Config} from 'protractor';
+
+export let config: Config = {
+  framework: 'jasmine',
+  capabilities: {
+    browserName: 'firefox'
+  },
+  
+suites: {
+        "spec01allPass.ts": 'spec01allPass.ts',
+	      "spec02allPass.ts": 'spec02allPass.ts'
+	},
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // You could set no globals to true to avoid jQuery '$' and protractor '$'
+  // collisions on the global namespace.
+  noGlobals: true
+};
